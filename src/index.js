1b76fdcdb76f1da6e693331885989fe2bb54553f
meilizheng <116130097+meilizheng@users.noreply.github.com>
2025-03-07 08:05:08 -0800

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+import {data} from "./data/data.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
